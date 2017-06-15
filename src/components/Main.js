52af49dd06519a513568e2e0c17cbd1bda239c52
CoolMonda <pikeman@pikemandeMacBook-Pro.local>
2017-06-15 21:47:56 +0800

    1

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,27 +2,32 @@ require('normalize.css/normalize.css');
 require('styles/App.css');
 
 import React from 'react';
-import { Link } from 'react-router-dom'
-
+import First from './first'
+import Second from './second'
+import { BrowserRouter, Route, Link ,Switch} from 'react-router-dom'
 
 // let yeomanImage = require('../images/yeoman.png');
 
 
 class AppComponent extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
   }
   render() {
-    console.log(this.props.children)
     return (
       <div className="index">
-        <ul>
-          <li><Link to="/first">first</Link></li>
-        <li><Link to="/second">second</Link></li>
-        </ul>
-        <div>
-          {this.props.children}
-        </div>
+        <BrowserRouter>
+          <div>
+              <ul>
+                <li><Link to="/first">first</Link></li>
+                <li><Link to="/second">Inbox</Link></li>
+              </ul>
+             <Switch>
+               <Route path="/first" component={First} />
+               <Route path="/second" component={Second} />
+             </Switch>
+           </div>
+         </BrowserRouter>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,23 +3,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/Main';
 
-import First from './components/first/index'
-import Second from './components/second/index'
-// import { Router, Link } from 'react-router'
-import { BrowserRouter, Route,Switch } from 'react-router-dom'
-
 // Render the main component into the dom
 ReactDOM.render(
-  // <App />,
-  (
-    <BrowserRouter>
-      <div>
-        <Switch>
-          <Route path="/first" component={First} />
-          <Route path="/second" component={Second} />
-        </Switch>
-      </div>
-    </BrowserRouter>
-  ),
+  <App />,
    document.getElementById('app')
  );
